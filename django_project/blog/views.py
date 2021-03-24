37f5b2fb4e02108c664be55c439488462a7da2d1
oritn <orit0261>
2021-03-24 15:25:00 +0200

    update

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,32 +1,13 @@
-from datetime import datetime
-import datetime as dt
-
-
 from django.shortcuts import render
 from .models import Post
 
-#from django.http import HttpResponse
-# posts = [
-#     {'author': 'Orit Naor',
-#      'title': 'Blog Post 1',
-#      'content': 'First post content',
-#      'date_posted': datetime.now()
-#      },
-#     {'author': 'Jane doe',
-#      'title': 'Blog Post 2',
-#      'content': 'Second post content',
-#      'date_posted': datetime.now()+dt.timedelta(days=+1)
-#      }
-#
-# ]
-# Create your views here.
+
 def home(request):
-  #context = {'posts':posts}
-  context = {'posts': Post.objects.all()}
-  return render(request,'blog/home.html',context)
-# return HttpResponse('<h1>Blog home</h1>')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html',{'title' : 'About'})
-    #return HttpResponse('<h1>Blog about</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
